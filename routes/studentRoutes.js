95e121087fa8755448674404c90f1e3d03e28ac3
Muhammad Usman <awanask086@gmail.com>
2026-08-07 10:38:17 +0500

    routes

---
+++ routes/studentRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const {handlegetAllstudent,handlegetStudentById,
+  handlegetStudnetBySearch,handleUpdateUserByPatch,
+hanldeCreteStudent,deleteStudentById,handleUpdateStudent} = require("../controller/studentController")
+const router = express.Router()
+// let users = [];
+//get all users
+
+
+router.route("/").get(handlegetAllstudent).post(hanldeCreteStudent);
+router.route("/search").get(handlegetStudnetBySearch)
+router.route("/:id").patch(handleUpdateUserByPatch).delete(deleteStudentById).put(handleUpdateStudent).get(handlegetStudentById);
+
+
+module.exports = router
+
